dc7a98611b870bbaba34bdc95c9c6cea835b5872
Jesse Maingi <maingijesse@gmail.com>
2020-07-02 14:35:07 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Simple Starter Kit
+
+A simple starter kit for HTMl css and parcel
+
+## Includes
+
+- Parcel JS
+- Post HTML for partial includes
+- PostCSS
---
+++ src/js/main.js
@@ -0,0 +1,29 @@
+const frontEndStack = [
+  "Html",
+  "Css",
+  "Javascript",
+  "Bootstrap",
+  "Materialize",
+  "jQuery",
+  "Vue",
+  "Vuex",
+  "Vuetify",
+  "Inkline",
+];
+
+frontEndStack.forEach((stack) => {
+  console.log(`FrontEnd Tech: ${stack}✨🚀`);
+});
+
+const backEndStack = [
+  "Node JS",
+  "MongoDB",
+  "Mongoose",
+  "Express",
+  "Adonis",
+  "HandleBars",
+];
+
+for (const stack of backEndStack) {
+  console.log(`BackEnd Tech: ${stack} 🌚🌛`);
+}
